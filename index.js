0a7c350b3833ab378b34adb859d7b75527540c60
Kibeom Kim <terr1976@hotmail.com>
2016-10-14 00:58:00 -0400

    Add file upload function

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+var upload = multer({ storage: _storage })
 var fs = require('fs');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
+app.use('/user', express.static('uploads'));
 app.set('views', './views');
 app.set('view engine', 'jade');
+app.get('/upload', function(req, res){
+  res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res){
+  console.log(req.file);
+  res.send('uploaded: ' + req.file.originalname);
+})
 app.get('/topic/new', function(req, res){
   fs.readdir('data', function(err, files){
     if(err){
@@ -14,7 +32,7 @@ app.get('/topic/new', function(req, res){
     }
     res.render('new', {topics:files});    
   });  
-})
+});
 app.get(['/topic', '/topic/:id'], function(req,res){
   fs.readdir('data', function(err, files){
     if(err){
@@ -36,8 +54,8 @@ app.get(['/topic', '/topic/:id'], function(req,res){
       res.render('view', {topics:files, title:'Welcome', description:'Javascript for Server'});    
     }
 
-  })
-})
+  });
+});
 app.post('/topic', function(req, res){
   var title = req.body.title;
   var description = req.body.description;
@@ -48,8 +66,7 @@ app.post('/topic', function(req, res){
     }
     res.redirect('/topic/' + title);    
   })
-})
-
+});
 app.listen(3000, function(){
   console.log('Connected 3000 port');
-})
+});
