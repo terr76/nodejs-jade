73f113809c4d2c6a63b51ed58416f6147a608dc2
Kibeom Kim <terr1976@hotmail.com>
2016-10-13 22:09:38 -0400

    Add file read/write function

--- index.js
+++ index.js
@@ -1,76 +1,52 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
-app.set('view engine', 'jade');
 app.set('views', './views');
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.get('/form', function(req, res){
-  res.render('form');
-});
-app.get('/form_receiver', function(req, res){
-  var title = req.query.title;
-  var description = req.query.description;
-  res.send(title+','+description);
-});
-app.post('/form_receiver', function(req, res){
+app.set('view engine', 'jade');
+app.get('/topic/new', function(req, res){
+  res.render('new');
+})
+app.get(['/topic', '/topic/:id'], function(req,res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err)
+      res.status(500).send('Internet Server Error');
+    }
+    res.render('view', {topics:files});    
+  })
+})
+app.get('/topic/:id', function(req, res){
+  var id = req.params.id;
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err)
+      res.status(500).send('Internet Server Error');
+    }
+    fs.readFile('data/' + id, 'utf-8', function(err, data){
+      if(err){
+        console.log(err);
+        res.status(500).send('Internet Server Error');      
+      }
+      res.render('view', {topics:files, title:id, description:data});
+    })    
+  })
+
+})
+app.post('/topic', function(req, res){
   var title = req.body.title;
   var description = req.body.description;
-  res.send(title+','+description);
-});
-app.get('/topic/:id', function(req, res){
-  var topics = [
-    'Javascript is....',
-    'Nodejs is...',
-    'Express is...'
-  ];
-  var output = `
-  <a href="/topic/0">JavaScript</a><br>
-  <a href="/topic/1">Nodejs</a><br>
-  <a href="/topic/2">Express</a><br><br>
-  ${topics[req.params.id]}
-  `
-  res.send(output);
+  fs.writeFile('data/' + title, description, function(err){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internet Server Error');
+    }
+    res.send('success');    
+  })
 })
-app.get('/topic/:id/:mode', function(req, res){
-  res.send(req.params.id+','+req.params.mode)
-})
-app.get('/template', function(req, res){
-  res.render('temp', {time:Date(), title:'Jade'});
-})
-app.get('/', function(req, res){
-    res.send('Hello home page');;
-});
-app.get('/dynamic', function(req, res){
-  var lis = '';
-  for(var i=0; i<5; i++){
-    lis = lis + '<li>coding</li>';
-  }
-  var time = Date();
-  var output = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title></title>
-    </head>
-    <body>
-        Hello, Dynamic!
-        <ul>
-          ${lis}
-        </ul>
-        ${time}
-    </body>
-  </html>`;
-  res.send(output);
-});
-app.get('/route', function(req, res){
-    res.send('Hello Router, <img src="/logo-mobile-m.png">');
-})
-app.get('/login', function(req, res){
-    res.send('<h1>Login please</h1>');
-});
+
 app.listen(3000, function(){
-    console.log('Conneted 3000 port!');
-});
+  console.log('Connected 3000 port');
+})
