b54efa4a6c173370832fbc94f7097a9154995d28
Kibeom Kim <terr1976@hotmail.com>
2016-10-15 00:49:40 -0400

    Add simple cart feature

--- index_cookie.js
+++ index_cookie.js
@@ -19,6 +19,42 @@ app.get('/products', function(req, res){
   }
   res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart/">Cart</a>`);
 });
+/*
+cart = {
+  1:2,
+  2:1
+}
+*/
+app.get('/cart/:id', function(req, res){
+  var id = req.params.id;
+  if(req.cookies.cart){
+    var cart = req.cookies.cart;
+  } else {
+    var cart = {};
+  }
+  if(!cart[id]){
+    cart[id] = 0;
+  } 
+  cart[id] = parseInt(cart[id]) + 1;
+  res.cookie('cart', cart);
+  res.redirect('/cart');
+});
+
+app.get('/cart', function(req, res){
+  var cart = req.cookies.cart;
+  if(!cart){
+    res.rend('Empty');
+  } else {
+    var output = '';
+    for(var id in cart){
+      output += `<li>${products[id].title} (${cart[id]})</li>`;
+    }
+  }
+  res.send(`
+    <h1>Cart</h1>
+    <ul>${output}</ul><a href="/products">Products</a>`);
+});
+
 
 app.get('/count', function(req, res){
   if(req.cookies.count){
