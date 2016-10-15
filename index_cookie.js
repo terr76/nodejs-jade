a21195f475fc82424f76548b6534a9da42992c64
Kibeom Kim <terr1976@hotmail.com>
2016-10-15 00:25:48 -0400

    cookie test

---
+++ index_cookie.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var app = express();
+app.use(cookieParser());
+
+var products = {
+  1:{title:'The history of web 1'},
+  2:{title:'The next web'}
+};
+
+app.get('/products', function(req, res){
+  var output = '';
+  for(var name in products){
+    output += `
+      <li>
+        <a href="/cart/${name}">${products[name].title}</a>
+      </li>`;
+    console.log(products[name].title);
+  }
+  res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart/">Cart</a>`);
+});
+
+app.get('/count', function(req, res){
+  if(req.cookies.count){
+    var count = parseInt(req.cookies.count);
+  } else {
+    var count = 0;
+  }
+  count = count + 1;
+  res.cookie('count', count);
+  res.send('count :' + count);
+});
+app.listen(3003, function(){
+  console.log('Connected 3003 port');
+});
